54702cd4c6ade58dbe534529c839593197648271
Dimon14101010 <dmitriy.nekrasov89>
2017-07-10 15:14:10 +0300

    img api erorr

--- app/getapi.model.js
+++ app/getapi.model.js
@@ -10,8 +10,8 @@
     function apiGet($resource) {
 
         let api = {
-            apiId : $resource('https://api.foursquare.com/v2/venues/explore?venuePhotos=1&v=20131016&ll=48.414067%2C35.126678&section=food&novelty=new&client_id=DCSSER23PTEWEWDETAWAYC2EMULS4VVOW1QFWFON2EDURAQR&client_secret=2QGX15UNLUGMFLEGYK5TAVT04XOBDE0DS10MJGID4ALV5C5B'),
-            apiPhoto : $resource('https://api.foursquare.com/v2/venues/:id/photos?v=20131016&client_id=DCSSER23PTEWEWDETAWAYC2EMULS4VVOW1QFWFON2EDURAQR&client_secret=2QGX15UNLUGMFLEGYK5TAVT04XOBDE0DS10MJGID4ALV5C5B',{id: '@id'})
+            apiId : $resource('https://api.foursquare.com/v2/venues/explore?venuePhotos=1&v=20131016&ll=48.414067%2C35.126678&section=food&novelty=new&client_id=MCYZZWIG0EVIPENVBVH4TOW3302YFRE5SUCGP0ECVTXG3MBE&client_secret=VBDZI0GECFF24FKACQW12L15JR5VEKUK0RZZTV51QJ0XYY3G'),
+            apiPhoto : $resource('https://api.foursquare.com/v2/venues/:id/photos?v=20131016&client_id=MCYZZWIG0EVIPENVBVH4TOW3302YFRE5SUCGP0ECVTXG3MBE&client_secret=VBDZI0GECFF24FKACQW12L15JR5VEKUK0RZZTV51QJ0XYY3G',{id: '@id'})
         }
 
     return api;
