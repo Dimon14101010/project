84b3a8e709259a3a75e2c74489611def931348d7
Dimon14101010 <dmitriy.nekrasov89>
2017-07-10 11:27:09 +0300

    img api erorr

--- app/controllers/dashboard.controller.js
+++ app/controllers/dashboard.controller.js
@@ -13,10 +13,15 @@
 
         vm.model = {
 
-            apiResponse: apiGet.apiId.get(),
-
-            apiResponsePhoto : apiGet.apiPhoto.get()
+            apiResponse: []
         };
+
+        apiGet.apiId.get().$promise.then(function(response) {
+            vm.model.apiResponse = response;
+            vm.model.apiResponse.response.groups[0].items.forEach(function(venue) {
+                venue.itsPhotos = apiGet.apiPhoto.get({id: venue.venue.id})
+            })
+        })
 
     };
 
--- app/getapi.model.js
+++ app/getapi.model.js
@@ -10,8 +10,8 @@
     function apiGet($resource) {
 
         let api = {
-            apiId : $resource('https://api.foursquare.com/v2/venues/explore?v=20131016&ll=48.414067%2C35.126678&section=food&novelty=new&client_id=AKQNZODERYZOMZKGMRGKOQLYGDXQCQPKTY3LS5QCPDEEJ3VK&client_secret=RO1TAM5TEW2GKM54LLC2VSYQEM5JOJOILVKQXMFNZ2A1V4YT'),
-            apiPhoto : $resource('https://api.foursquare.com/v2/venues/5478a469498eb0f50cd9b052/photos?v=20131016&client_id=AKQNZODERYZOMZKGMRGKOQLYGDXQCQPKTY3LS5QCPDEEJ3VK&client_secret=RO1TAM5TEW2GKM54LLC2VSYQEM5JOJOILVKQXMFNZ2A1V4YT',{id: '@id'})
+            apiId : $resource('https://api.foursquare.com/v2/venues/explore?venuePhotos=1&v=20131016&ll=48.414067%2C35.126678&section=food&novelty=new&client_id=AKQNZODERYZOMZKGMRGKOQLYGDXQCQPKTY3LS5QCPDEEJ3VK&client_secret=RO1TAM5TEW2GKM54LLC2VSYQEM5JOJOILVKQXMFNZ2A1V4YT'),
+            apiPhoto : $resource('https://api.foursquare.com/v2/venues/:id/photos?v=20131016&client_id=AKQNZODERYZOMZKGMRGKOQLYGDXQCQPKTY3LS5QCPDEEJ3VK&client_secret=RO1TAM5TEW2GKM54LLC2VSYQEM5JOJOILVKQXMFNZ2A1V4YT',{id: '@id'})
         }
 
     return api;
